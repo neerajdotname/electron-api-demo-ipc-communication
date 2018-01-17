d174fed92421052ac0e671d6848fa703b66ab9b5
Neeraj Singh <neeraj@bigbinary.com>
2018-01-17 14:43:44 -0800

    wip

--- webview-injecting-javascript-and-css/preload.js
+++ webview-injecting-javascript-and-css/preload.js
@@ -1,13 +1,39 @@
+// Not all applications will have jQuery.
+// BigBinary website has jQuery.
+// Hence following code is working
+// try to handle following code without using jQuery
+
+const handleMouseMove = (event) => {
+  const $el = $(event.target);
+  const selector1 = "selector1"
+  $el.attr("aria-label",selector1);
+  $el.addClass("hint--bottom"); //needed by Hint.css
+  $el.addClass("hintBox"); //added by BigBinary
+}
+
+const handleMouseOut = (event) => {
+  const $el = $(event.target);
+  $el.removeClass("hint--bottom");
+  $el.removeClass("hintBox");
+}
+
+const addTracking = () => {
+    $('*').on('mousemove', handleMouseMove);
+    $('*').on('mouseout', handleMouseOut);
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    var script = document.createElement("script");
-    script.src = "https://unpkg.com/tippy.js@2.0.9/dist/tippy.all.min.js";
-    script.onload = script.onreadystatechange = function() {
-        $(function() {
-          var selector = "#cityName";
-          var el = $(selector);
-          el.attr("title", "my tooltip");
-          tippy(selector);
-        });
-    };
-    document.body.appendChild(script);
+
+    var styles = document.createElement('link');
+    styles.rel = 'stylesheet';
+    styles.type = 'text/css';
+    styles.media = 'screen';
+    styles.href = 'https://s3.amazonaws.com/zindi-io/hint.min.css';
+
+    document.body.appendChild(styles);
+
+  // Was not able to figure out when jQuery is fully loaded
+    setTimeout(addTracking, 2000);
+
 });
